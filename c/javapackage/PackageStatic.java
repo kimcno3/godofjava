af8957a3c963f56e558e01d124486537f7ebc844
kimcno3 <77563468+kimcno3@users.noreply.github.com>
2022-01-20 16:44:28 +0900

    Ch9 - import static 예제

---
+++ c/javapackage/PackageStatic.java
@@ -0,0 +1,18 @@
+package c.javapackage;
+
+// import c.javapackage.sub.SubStatic;
+
+// import static c.javapackage.sub.SubStatic.subStaticMethod;
+// import static c.javapackage.sub.SubStatic.CLASS_NAME;
+
+import static c.javapackage.sub.SubStatic.*;
+
+public class PackageStatic{
+    public static void main(String[] args){
+        // SubStatic.subStaticMethod();
+        // System.out.println(SubStatic.CLASS_NAME);
+        
+        subStaticMethod();
+        System.out.println(CLASS_NAME);   
+    }
+}
