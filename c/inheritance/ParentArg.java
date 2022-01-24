5b05dd5421bdcf37c01ecdcd94b246148057a12a
kimcno3 <77563468+kimcno3@users.noreply.github.com>
2022-01-24 17:12:27 +0900

    Ch10 상속 super 메소드 예제

---
+++ c/inheritance/ParentArg.java
@@ -0,0 +1,10 @@
+package c.inheritance;
+
+public class ParentArg{
+    public ParentArg(String name){
+        System.out.println("ParentArg(" + name + ") Constructor");
+    }
+    public void printName(){
+        System.out.println("printName() - printArg");
+    }
+};
