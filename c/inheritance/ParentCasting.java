8d15cbcbc4c5eb02d79a1c62b9c02b8e353bd97e
kimcno3 <77563468+kimcno3@users.noreply.github.com>
2022-01-25 01:29:59 +0900

    Ch10 형변환 예제

---
+++ c/inheritance/ParentCasting.java
@@ -0,0 +1,11 @@
+package c.inheritance;
+
+public class ParentCasting {
+    public ParentCasting(){
+    }
+    public ParentCasting(String name){
+    }
+    public void printName(){
+        System.out.println("printName() - parent");
+    }
+}
