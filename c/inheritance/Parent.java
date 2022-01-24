32a1cef1dc5d0b8f7083b488a02af459d55c4434
kimcno3 <77563468+kimcno3@users.noreply.github.com>
2022-01-24 18:08:59 +0900

    주석문 제거

--- c/inheritance/Parent.java
+++ c/inheritance/Parent.java
@@ -1,9 +1,9 @@
 package c.inheritance;
 
 public class Parent{
-    // public Parent(){
-    //     System.out.println("Parent Constructor");
-    // }
+    public Parent(){
+        System.out.println("Parent Constructor");
+    }
     public void printName(){
         System.out.println("Parent printName()");
     }
