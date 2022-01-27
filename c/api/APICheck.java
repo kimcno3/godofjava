40a32c530742a2c82eadb4ae98bb8275d27b0d6a
kimcno3 <77563468+kimcno3@users.noreply.github.com>
2022-01-27 16:35:28 +0900

    Ch11 Deprecated 메소드 예제
    - api 패키지 생성
    - String 클래스의 getBytes 메소드 실행

---
+++ c/api/APICheck.java
@@ -0,0 +1,13 @@
+package c.api;
+
+public class APICheck {
+    public static void main(String[] args) {
+        APICheck api = new APICheck();
+        api.useDeprecated();
+    }
+    public void useDeprecated(){
+        String str = "abcde";
+        byte[] strBytes = str.getBytes(); // Deprecated Method of String Class
+        String convertedStr = new String(strBytes,0); // signal of deprecated Method
+    }
+}
