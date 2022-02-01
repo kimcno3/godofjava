178c196ea61ca392b5da2cb9206211448f0b04ce
kimcno3 <77563468+kimcno3@users.noreply.github.com>
2022-02-01 22:16:06 +0900

    Ch12 hashcode() 예제 메소드 추가

--- c/inheritance/Equals.java
+++ c/inheritance/Equals.java
@@ -6,7 +6,8 @@ public class Equals {
     public static void main(String[] args){
         Equals thisObject = new Equals();
         // thisObject.equalMethod();
-        thisObject.equalMethod2();
+        // thisObject.equalMethod2();
+        thisObject.hashcodeMethod();
     }
     public void equalMethod(){
         MemberDTO obj1 = new MemberDTO("Sangmin");
@@ -24,6 +25,15 @@ public class Equals {
             System.out.println("Two obj is same");
         } else {
             System.out.println("Two obj is different");
+        }
+    }
+    public void hashcodeMethod(){
+        MemberDTO obj1 = new MemberDTO("Sangmin");
+        MemberDTO obj2 = new MemberDTO("Sangmin");
+        if (obj1.hashCode() == obj2.hashCode()){
+            System.out.println("Two obj is same " + obj1.hashCode() + ", " + obj2.hashCode());
+        } else {
+            System.out.println("Two obj is different " + obj1.hashCode() + ", " + obj2.hashCode());
         }
     }
 }
