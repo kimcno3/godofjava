0740fefec1e5b84977143206c863918e94f1a7de
kimcno3 <77563468+kimcno3@users.noreply.github.com>
2022-02-03 16:53:40 +0900

    주석처리

--- c/InterfaceExample.java
+++ c/InterfaceExample.java
@@ -2,7 +2,7 @@ package c;
 
 import c.service.MemberManager;
 import c.service.MemberManagerImpl;
-import c.service.MemberManagerImpl2;
+// import c.service.MemberManagerImpl2;
 
 public class InterfaceExample {
     public static void main(String[] args) {
@@ -10,8 +10,8 @@ public class InterfaceExample {
         boolean result = member.removeMember("Kim", "010-xxxx-xxxx");
         System.out.println(result);
 
-        MemberManager member2 = new MemberManagerImpl2();
-        result = member2.removeMember("Kim", "010-xxxx-xxxx");
-        System.out.println(result);
+        // MemberMangerAbstract member2 = new MemberManagerImpl2();
+        // result = member2.removeMember("Kim", "010-xxxx-xxxx");
+        // System.out.println(result);
     }
 }
